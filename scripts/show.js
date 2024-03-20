04424607b2c002e5b88fc4ff897d39dd96af49ed
hulrap <49691381+hulrap@users.noreply.github.com>
2024-03-20 11:22:48 +0100

    Add files via upload

---
+++ scripts/show.js
@@ -0,0 +1,6 @@
+JavaScript (script.js):
+
+    document.addEventListener("click", function() { 
+      var hiddenText = document.getElementById("hiddenText"); 
+      hiddenText.style.display = "block"; 
+    }); // JavaScript Document
